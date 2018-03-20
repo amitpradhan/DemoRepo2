ee925257e7c79cf5c1c0900a949aceaa260ddc8a
Amit Kumar <amitpradhan.2011@gmail.com>
2018-03-21 02:52:44 +0530

    Second commit

--- src/main/java/com/qa/pages/HomPage.java
+++ src/main/java/com/qa/pages/HomPage.java
@@ -5,4 +5,8 @@ public class HomPage {
 	public void getHomepageTitle(){
 		System.out.println("Home page title is--");
 	}
+	
+	public void getHomepageMenu(){
+		System.out.println("Home page Menu is--");
+	}
 }
